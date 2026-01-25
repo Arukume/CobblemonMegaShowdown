e7c64feb98a1c8d276c745da1ed75f80ec2f57db
yajatkaul <mc.crafter1973@gmail.com>
2026-01-25 23:51:35 +0530

    Removed unneeded mixins

--- common/src/main/java/com/github/yajatkaul/mega_showdown/utils/duck/cobblemon/interfaces/ClientBattleDuck.java
+++
@@ -1,7 +0,0 @@
-package com.github.yajatkaul.mega_showdown.utils.duck.cobblemon.interfaces;
-
-import com.cobblemon.mod.common.battles.ShowdownMoveset;
-
-public interface ClientBattleDuck {
-    boolean mega_showdown$pendingGimmick(ShowdownMoveset.Gimmick gimmick);
-}
