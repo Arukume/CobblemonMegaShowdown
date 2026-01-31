2b0c2894e398a6e2b0193696352beb002ec17fbd
yajatkaul <mc.crafter1973@gmail.com>
2026-01-31 15:31:47 +0530

    Grade update

--- common/src/main/java/com/github/yajatkaul/mega_showdown/mixin/battle/GimmickTileMixin.java
+++ common/src/main/java/com/github/yajatkaul/mega_showdown/mixin/battle/GimmickTileMixin.java
@@ -6,9 +6,7 @@ import com.cobblemon.mod.common.battles.ShowdownMoveset;
 import com.cobblemon.mod.common.client.gui.battle.subscreen.BattleGimmickButton;
 import com.cobblemon.mod.common.client.gui.battle.subscreen.BattleMoveSelection;
 import com.cobblemon.mod.common.util.math.SimpleMathExtensionsKt;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,19 +14,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = BattleGimmickButton.GimmickTile.class, remap = false)
 public class GimmickTileMixin {
-    @Unique
-    protected BattleMoveSelection moveSelection;
-    @Unique
-    protected InBattleMove move;
-    @Final
-    @Shadow
-    private ShowdownMoveset.Gimmick gimmick;
+    @Unique protected BattleMoveSelection mega_showdown$moveSelection;
+    @Unique protected InBattleMove mega_showdown$move;
 
     @Inject(method = "<init>", at = @At("TAIL"), remap = false)
     private void init(ShowdownMoveset.Gimmick gimmick, BattleMoveSelection moveSelection, InBattleMove move, float x, float y, CallbackInfo ci) {
         BattleGimmickButton.GimmickTile tile = (BattleGimmickButton.GimmickTile) (Object) this;
-        this.moveSelection = tile.getMoveSelection();
-        this.move = tile.getMove();
+        this.mega_showdown$moveSelection = tile.getMoveSelection();
+        this.mega_showdown$move = tile.getMove();
 
         if (gimmick == ShowdownMoveset.Gimmick.TERASTALLIZATION || gimmick == ShowdownMoveset.Gimmick.MEGA_EVOLUTION) {
             tile.setMoveTemplate(Moves.getByNameOrDummy(move.id));
